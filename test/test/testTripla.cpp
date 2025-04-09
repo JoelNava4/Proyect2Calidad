858d99c5352b3d4add810958f86afcbb77e644af
joel.navarro@ucb.edu.bo <joel.navarro@ucb.edu.bo>
2025-04-09 15:18:10 -0400

    Prueba unitaria para probar push y get lista de clase Tripla.h

--- test/test/testTripla.cpp
+++ test/test/testTripla.cpp
@@ -6,3 +6,13 @@ TEST(TriplaTest, SetAndGetElem) {
     t.setElem(42);
     EXPECT_EQ(t.getElem(), 42);
 }
+
+TEST(TriplaTest, PushGetLista) {
+    Tripla<string> t;
+    t.pushLista("Joel");
+    t.pushLista("Nava");
+    vector<string> lista = t.getLista();
+    EXPECT_EQ(lista.size(), 2);
+    EXPECT_EQ(lista[0], "Joel");
+    EXPECT_EQ(lista[1], "Nava");
+}
